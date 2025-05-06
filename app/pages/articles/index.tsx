901d6005c01cdb77052bd41e15c4111a3ff73cfe
Sivtout666 <maxime.giraudo@free.fr>
2025-05-06 23:16:29 +0200

    Base des articles + header + footer

--- app/pages/articles/index.tsx
+++ app/pages/articles/index.tsx
@@ -1,17 +1,24 @@
 import Lien from "~/components/Lien";
+import Header from "~/components/header"
+import Footer from "~/components/footer"
 
-export default () => <div className="p-5">
-    <p>
-        /articles
-    </p>
-    <p>
-        On liste tous les articles sur cette page
-    </p>
+export default () => 
+    <div>
+        <Header/>
+        <div className="p-5">
+            <p>
+                /articles
+            </p>
+            <p>
+                On liste tous les articles sur cette page
+            </p>
 
-    <Lien to={`/articles/` + Math.floor(Math.random() * 9999999999).toString(36)} >
-        aller à un article random
-    </Lien>
-    <Lien to="/">
-        aller à la page d'accueil
-    </Lien>
-</div>
+            <Lien to={`/articles/` + Math.floor(Math.random() * 9999999999).toString(36)} >
+                aller à un article random
+            </Lien>
+            <Lien to="/">
+                aller à la page d'accueil
+            </Lien>
+        </div>
+        <Footer/>
+    </div>
