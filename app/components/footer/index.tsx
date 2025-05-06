ce81f759d441412d05533bf7f0692e785d4f2b20
Sivtout666 <maxime.giraudo@free.fr>
2025-05-06 23:16:29 +0200

    Base des articles + header + footer

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,32 @@
+
+
+function Footer () {
+    return (<>
+        <div className="w-svw h-50 bg-gray-100 grid grid-cols-3">
+            <div className="p-5 grid grid-rows-5 gap-1">
+                <div className="bg-amber-100"></div>
+                <div className="bg-amber-200"></div>
+                <div className="bg-amber-300"></div>
+                <div className="bg-amber-400"></div>
+                <div className="bg-amber-500"></div>
+            </div>
+            <div className="p-5 grid grid-rows-5 gap-1">
+                <div className="bg-amber-100"></div>
+                <div className="bg-amber-200"></div>
+                <div className="bg-amber-300"></div>
+                <div className="bg-amber-400"></div>
+                <div className="bg-amber-500"></div>
+            </div>
+            <div className="p-5 grid grid-rows-5 gap-1">
+                <div className="bg-amber-100"></div>
+                <div className="bg-amber-200"></div>
+                <div className="bg-amber-300"></div>
+                <div className="bg-amber-400"></div>
+                <div className="bg-amber-500"></div>
+            </div>
+        </div>
+    
+    </>)
+}
+
+export default Footer;
