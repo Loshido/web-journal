a8725d627287644fb8fa72b2d07992b9e8c26eaa
loshido <loshido@pm.me>
2025-05-07 14:06:37 +0200

    Correction composants header & footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,32 +1,25 @@
-
-
-function Footer () {
-    return (<>
-        <div className="w-svw h-50 bg-gray-100 grid grid-cols-3">
-            <div className="p-5 grid grid-rows-5 gap-1">
-                <div className="bg-amber-100"></div>
-                <div className="bg-amber-200"></div>
-                <div className="bg-amber-300"></div>
-                <div className="bg-amber-400"></div>
-                <div className="bg-amber-500"></div>
-            </div>
-            <div className="p-5 grid grid-rows-5 gap-1">
-                <div className="bg-amber-100"></div>
-                <div className="bg-amber-200"></div>
-                <div className="bg-amber-300"></div>
-                <div className="bg-amber-400"></div>
-                <div className="bg-amber-500"></div>
-            </div>
-            <div className="p-5 grid grid-rows-5 gap-1">
-                <div className="bg-amber-100"></div>
-                <div className="bg-amber-200"></div>
-                <div className="bg-amber-300"></div>
-                <div className="bg-amber-400"></div>
-                <div className="bg-amber-500"></div>
-            </div>
+export default function Footer() {
+    return <footer className="w-svw h-50 bg-gray-100 grid grid-cols-3">
+        <div className="p-5 grid grid-rows-5 gap-1">
+            <div className="bg-amber-100"></div>
+            <div className="bg-amber-200"></div>
+            <div className="bg-amber-300"></div>
+            <div className="bg-amber-400"></div>
+            <div className="bg-amber-500"></div>
         </div>
-    
-    </>)
+        <div className="p-5 grid grid-rows-5 gap-1">
+            <div className="bg-amber-100"></div>
+            <div className="bg-amber-200"></div>
+            <div className="bg-amber-300"></div>
+            <div className="bg-amber-400"></div>
+            <div className="bg-amber-500"></div>
+        </div>
+        <div className="p-5 grid grid-rows-5 gap-1">
+            <div className="bg-amber-100"></div>
+            <div className="bg-amber-200"></div>
+            <div className="bg-amber-300"></div>
+            <div className="bg-amber-400"></div>
+            <div className="bg-amber-500"></div>
+        </div>
+    </footer>
 }
-
-export default Footer;
