4da37d5855a772a3cb8b358dcde4711b6ef29686
Tomreichling <tomreichling83@gmail.com>
2025-05-18 20:39:32 +0200

    V1 systeme likes

--- app/pages/articles/article.tsx
+++ app/pages/articles/article.tsx
@@ -1,8 +1,11 @@
 import data from "~/../data/articles.json";
 import Lien from "~/components/Lien";
 import { useParams } from "react-router";
-
+import PouceUp from "~/components/icons/pouce1";
+import PouceDown from "~/components/icons/pouce2";
 import "~/components/articles/article.css"
+
+
 export default () => {
   const { article: id } = useParams();
   const article = data.find((a) => a.id === id);
@@ -10,7 +13,7 @@ export default () => {
   if (!article) {
     return (
       <div className="text-center mt-20 text-3xl text-red-600">
-        Ahhhh Article introuvable !! Comment on fait ?
+        Ahhhh Article introuvable ! Comment on fait ?
       </div>
     );
   }
@@ -28,11 +31,11 @@ export default () => {
           </div>
         </div>
 
-        {/* <img
-          src={article.image }
-          alt={`Illustration pour ${article.titre}`}
+        <img
+          src={article.image}
+          alt={"image impossible à charger"}
           className="w-full rounded-lg shadow-md mb-6"
-        /> */}
+        />
 
         <div
           className="mb-4 article"
@@ -44,7 +47,10 @@ export default () => {
         </p>
       </div>
 
-      <Lien to="/articles">
+
+  
+          <div className="flex justify-around items-center">
+        <Lien to="/articles">
         <div className="flex items-center gap-2 bg-blue-400 hover:bg-blue-500 text-white px-4 py-2 rounded-2xl w-fit transition-colors">
           <svg
             viewBox="0 0 24 24"
@@ -67,6 +73,36 @@ export default () => {
           Revenir aux articles
         </div>
       </Lien>
+
+          <div className="flex items-center bg-gray-100 rounded-3xl w-fit p-1 overflow-hidden">
+      <div className="flex items-center">
+        <button 
+          
+          className="flex items-center px-3 py-2 gap-2  rounded-l-3xl hover:bg-gray-200 "
+        >
+          <PouceUp 
+            className="w-5 h-5  fill-blue-400 stroke-1 stroke-gray-200"
+          />
+          <p className="text-sm font-medium text-blue-400">{article.reaction.likes}</p>
+        </button>
+        
+        <div className="w-px h-5 bg-gray-300 mx-1"></div>
+        
+        <button 
+         
+          className="flex items-center px-3 py-2 gap-2 rounded-r-3xl hover:bg-gray-200 "
+        >
+          <PouceDown 
+            className="w-5 h-5 fill-red-600 stroke-1 stroke-gray-200"
+          />
+          <p className="text-sm font-medium text-red-600 ">{article.reaction.dislikes}</p>
+        </button>
+      </div>
+    </div>
+
+
+      </div>
+      
     </div>
   );
 };
