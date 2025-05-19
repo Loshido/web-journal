477b72f7d8be07c41ac38fd9f8a9fd45bbeb901d
Sivtout666 <maxime.giraudo@free.fr>
2025-05-19 20:44:42 +0200

    .

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Page({ loaderData: articles }: Route.ComponentProps) {
 
     return <div>
         <Header/>
-        <div className="mb-10 mt-10 flex flex-col items-center justify-center">
-            <div className="h-20 w-full flex items-center justify-center text-center font-semibold text-xl p-5 mb-10
-            md:text-3xl">Le site où chaque article éclaire, questionne et inspire.</div>
+        <section className="mb-10 mt-10 flex flex-col items-center justify-center">
+            <h1 className="h-20 w-full flex items-center justify-center text-center font-semibold text-xl p-5 mb-10
+            md:text-3xl">Le site où chaque article éclaire, questionne et inspire.</h1>
             <div className="bg-gray-100 rounded-2xl w-screen h-screen p-5 flex items-center justify-center
              md:w-8xl md:h-100">
                 <div className="w-screen h-full rounded-2xl grid grid-cols-1 gap-3 grid-rows-2
@@ -30,10 +30,10 @@ export default function Page({ loaderData: articles }: Route.ComponentProps) {
                     <div className=" col-span-3 rounded-2xl row-span-1">
                         <div className="w-full h-full rounded-2xl p-10 gap-7 grid grid-rows-2
                         md:grid-rows-5 md:gap-1">
-                            <div className="text-xl row-span-2 flex justify-center items-center pl-1
+                            <h2 className="text-xl row-span-2 flex justify-center items-center pl-1
                             sm:pl-5 sm:text-2xl md:text-4xl">
                                 {articles[0].titre}
-                            </div>
+                            </h2>
                             <div className="row-span-2 p-2
                             sm:p-5 md:text-xl">
                                 {articles[0].description}
@@ -44,9 +44,7 @@ export default function Page({ loaderData: articles }: Route.ComponentProps) {
                         </div>
                     </div>
                     <Lien to={`/articles/` + articles[0].id} className="bg-gray-200 col-span-2 rounded-2xl grid row-span-1">
-                        <div className="flex">
-                            <img className="rounded-2xl w-full h-full overflow-hidden object-cover" src={articles[0].image || "/assets/Bureau.jpeg"}/>
-                        </div>
+                        <img className="rounded-2xl w-full h-full overflow-hidden object-cover" src={articles[0].image || "/assets/Bureau.jpeg"}/>
                     </Lien>
                 </div>
             </div>
@@ -85,29 +83,31 @@ export default function Page({ loaderData: articles }: Route.ComponentProps) {
                     </Lien>
                 </div>
             </div>
+        </section>
+        <section>
             <div className="bg-gray-100 mt-10 mb-10 w-screen h-screen flex flex-col">
                 <div className="w-full h-full grid grid-cols-1 grid-rows-5
                 md:grid-cols-11 md:grid-rows-6">
-                    <div className="flex items-center justify-center col-span-1 row-span-1 text-lg
-                    md:font-semibold md:col-start-4 md:row-span-2 md:col-span-5 md:text-3xl">Comment sont faits nos articles ?</div>
+                    <h1 className="flex items-center justify-center col-span-1 row-span-1 text-lg
+                    md:font-semibold md:col-start-4 md:row-span-2 md:col-span-5 md:text-3xl">Comment sont faits nos articles ?</h1>
                     <div className="grid row-span-2
                      md:grid-cols-4 md:grid-rows-7 md:row-start-3 md:col-span-5 md:row-span-9 bg-gray-200">
                         <div className=" flex justify-center items-center p-5 text-center
                         md:col-span-4 md:row-start-1 md:row-span-4 md:text-2xl md:text-left md:pl-10">La grande majorité des articles que vous trouverez ici sont écrits par des personnes passionnées, qui mettent leur cœur et leur savoir dans chaque mot.</div>
-                        <div className="flex justify-center items-center text-center
-                        md:text-4xl md:col-span-2 md:row-start-5 md:row-span-2 md:col-start-1">HUMAIN</div>
+                        <h2 className="flex justify-center items-center text-center
+                        md:text-4xl md:col-span-2 md:row-start-5 md:row-span-2 md:col-start-1">HUMAIN</h2>
                     </div>
                     <Triangle className="w-fit h-full fill-gray-200 md:row-start-3 md:row-span-9 md:col-start-6 hidden md:flex" />
                     <div className="grid row-span-2
                      md:grid-cols-3 md:grid-rows-7 md:row-start-3 md:col-start-7 md:col-span-5 md:row-span-9">
-                        <div className=" flex justify-center items-center text-center flex-col
-                        md:col-span-2 md:row-start-2 md:col-start-2 md:text-4xl">CHATGPT</div>
+                        <h2 className=" flex justify-center items-center text-center flex-col
+                        md:col-span-2 md:row-start-2 md:col-start-2 md:text-4xl">CHATGPT</h2>
                         <div className=" flex justify-center items-center text-center p-5
                         md:col-span-3 md:row-start-3 md:row-span-5 md:text-2xl md:text-right md:p-10">Certains articles sont également rédigés avec l’aide de ChatGPT : il suffit que l'administrateur clique sur un bouton et donne une idée ou un axe, et l’article prend forme en quelques instants.</div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
         <Footer/>
     </div>
 }
