97ee1390d951a0219dbfa904a39ac36efbb79cab
Sivtout666 <maxime.giraudo@free.fr>
2025-05-20 22:59:56 +0200

    Ajustement du footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -2,12 +2,12 @@ import Lien from "../Lien"
 import Logo from "~/components/icons/logo"
 
 export default function Footer() {
-    return <footer className="w-svw bg-gray-100 grid grid-cols-1
-    md:grid-cols-4 md:h-auto">
+    return <footer className="w-svw h-auto bg-gray-100 grid grid-cols-1
+    md:grid-cols-4">
         <div className="flex flex-col gap-16 md:gap-0 md:grid w-full h-full md:grid-rows-2 md:grid-cols-2 md:col-span-3 relative">
             <div className="grid w-full col-span-2 row-span-1
             md:grid-cols-2 ">
-                <div className="grid grid-rows-3 p-5 pl-10">
+                <div className="grid grid-rows-2 p-5 pl-10">
                     <p>
                         Ce site est un projet de webdev. 
                         L’idée de ce projet est d’allier toutes nos connaissances dans ce domaine 
@@ -15,7 +15,7 @@ export default function Footer() {
                     </p>
                     <div className="flex row-span-1 items-center font-semibold">Livio A, Maxime G et Tom R</div>
                 </div>
-                <div className="grid col-span-1 grid-cols-2">
+                <div className="grid col-span-1 grid-col-1 md:grid-cols-2">
                     <div className="flex flex-col p-3 gap-2">
                         <h1 className="font-semibold flex items-center text-4xl">Pages</h1>
                         <div className="flex flex-col pl-4">
@@ -50,7 +50,7 @@ export default function Footer() {
             </div>
             <h1 className="flex w-full h-full text-8xl
              md:justify-start md:items-end md:row-span-1 md:row-start-2 col-span-2 
-             md:text-9xl pb-0 xl:text-[256px] xl:leading-[67%]">
+             sm:text-9xl pb-0 xl:text-[230px] xl:leading-[67%]">
                 LE BLOG
             </h1>   
         </div>
